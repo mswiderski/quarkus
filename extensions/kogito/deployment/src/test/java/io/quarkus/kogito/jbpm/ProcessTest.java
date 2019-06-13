95f77b98a8feeebc9f48f92e9a26c2ccbd0c6f94
mariofusco <mario.fusco@gmail.com>
2019-06-13 16:20:46 +0200

    remove copyright headers

--- extensions/kogito/deployment/src/test/java/io/quarkus/kogito/jbpm/ProcessTest.java
+++ extensions/kogito/deployment/src/test/java/io/quarkus/kogito/jbpm/ProcessTest.java
@@ -1,29 +1,11 @@
-/*
- * Copyright 2019 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.quarkus.kogito.jbpm;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import io.quarkus.test.QuarkusUnitTest;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
@@ -33,7 +15,7 @@ import org.kie.kogito.Model;
 import org.kie.kogito.process.Process;
 import org.kie.kogito.process.ProcessInstance;
 
-import io.quarkus.test.QuarkusUnitTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProcessTest {
 
